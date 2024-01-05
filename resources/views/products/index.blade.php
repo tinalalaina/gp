11610849222b764ac86f069248febbd6e4e01bcc
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-05 04:51:03 -1200

    installation de bootstrap

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,9 +1,26 @@
-<h1>Product list</h1>
-<br>
-<a href="/product2">Savon</a>
-<br>
-<a href="/product3">Cigarette</a>
-<br>
-<a href="/product4">Huille</a>
-<br>
-<a href="/personne/liste-personnes">photo facture</a>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Product list</h1>
+    <br>
+    <a href="/product2">Savon</a>
+    <br>
+    <a href="/product3">Cigarette</a>
+    <br>
+    <a href="/product4">Huille</a>
+    <br>
+    <a href="/personne/liste-personnes">photo facture</a>
+    
+
+<script src="{{asset('css/bootstrap.js')}}"></script>
+</body>
+</html>
