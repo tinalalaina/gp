1802e36988546dd9cfdcefa09bce1e0842bfc807
tinalalaina14 <tinalalaina14@gmail.com>
2024-01-07 16:10:43 -1200

    odification pour que tous les listes soit afficher

--- app/Http/Controllers/EnregistrementController.php
+++ app/Http/Controllers/EnregistrementController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product1;
 use App\Models\Product2;
+use App\Models\Product3;
+use App\Models\Product4;
+use App\Models\Product5;
+use App\Models\Product6;
+use App\Models\Product7;
+use App\Models\Product8;
+use App\Models\Product9;
 
 class EnregistrementController extends Controller
 {
@@ -12,6 +19,14 @@ class EnregistrementController extends Controller
     {
         $product1s = Product1::all();
         $product2s = Product2::all();
-        return view('enregistrements.index', compact('product1s', 'product2s'));
+        $product3s = Product3::all();
+        $product4s = Product4::all();
+        $product5s = Product5::all();
+        $product6s = Product6::all();
+        $product7s = Product7::all();
+        $product8s = Product8::all();
+        $product9s = Product9::all();
+        
+        return view('enregistrements.index', compact('product1s', 'product2s','product3s','product4s','product5s','product6s','product7s','product8s','product9s',));
     }
 }
--- resources/views/enregistrements/index.blade.php
+++ resources/views/enregistrements/index.blade.php
@@ -40,6 +40,117 @@
       </tbody>
     </table>
 
+    <h2>Liste des produit3s</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+            </tr>
+        </thead>
+        <tbody>
+    @foreach($product3s as $product3 )
+    <tr>
+        <td>{{$product3->name}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+
+  <h2>Liste des produit4s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product4s as $product4 )
+      <tr>
+          <td>{{$product4->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h2>Liste des produit5s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product5s as $product5 )
+      <tr>
+          <td>{{$product5->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h2>Liste des produit6s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product6s as $product6 )
+      <tr>
+          <td>{{$product6->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h2>Liste des produit7s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product7s as $product7 )
+      <tr>
+          <td>{{$product7->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h2>Liste des produit8s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product8s as $product8 )
+      <tr>
+          <td>{{$product8->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <h2>Liste des produit9s</h2>
+      <table>
+          <thead>
+              <tr>
+                  <th>Nom</th>
+              </tr>
+          </thead>
+          <tbody>
+      @foreach($product9s as $product9 )
+      <tr>
+          <td>{{$product9->name}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
 
 
     </div>
